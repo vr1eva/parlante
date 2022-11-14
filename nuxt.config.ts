6653a98f46332a2d065284b91279700e86206401
Bruno <113930888+vr1eva@users.noreply.github.com>
2022-11-13 21:37:46 -0500

    configured sanity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: ['@nuxtjs/sanity'],
+    sanity: {
+        projectId: '05j2s02h',
+    },
 })
